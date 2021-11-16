cc24915d7fab858b079c949b762bd8f2cfcb7d5a
andrey-aka-skif <2119163@gmail.com>
2021-11-16 23:58:09 +0300

    Todo завершено по туториалу

---
+++ Todo/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+using System.Reactive;
+using Todo.Models.Todo.Models;
+
+namespace Todo.ViewModels
+{
+    class AddItemViewModel : ViewModelBase
+    {
+        private string _description;
+
+        public string Description
+        {
+            get => _description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
+
+        public ReactiveCommand<Unit, TodoItem> Ok { get; }
+        public ReactiveCommand<Unit, Unit> Cancel { get; }
+
+        public AddItemViewModel()
+        {
+            var okEnabled = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            Ok = ReactiveCommand.Create(
+                () => new TodoItem { Description = Description },
+                okEnabled);
+            Cancel = ReactiveCommand.Create(() => { });
+        }
+    }
+}
--- Todo/ViewModels/MainWindowViewModel.cs
+++ Todo/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,46 @@
+using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Todo.Models;
+using Todo.Models.Todo.Models;
 using Todo.Services;
 
 namespace Todo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase _content;
+
         public TodoListViewModel List { get; }
+
+        public ViewModelBase Content
+        {
+            get => _content;
+            private set => this.RaiseAndSetIfChanged(ref _content, value);
+        }
 
         public MainWindowViewModel(Database db)
         {
             List = new TodoListViewModel(db.GetItems());
+            Content = List;
+        }
+
+        public void AddItem()
+        {
+            var vm = new AddItemViewModel();
+            Observable.Merge(
+                vm.Ok,
+                vm.Cancel.Select(_ => (TodoItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+                    Content = List;
+                });
+            Content = vm;
         }
     }
 }
